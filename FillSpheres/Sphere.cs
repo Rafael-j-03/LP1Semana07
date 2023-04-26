88ae889b1150d8b83d88e371dc6e552b376e56a8
Rafael José <ra.david@hotmail.com>
2023-04-26 14:10:36 +0100

    Add class Sphere
    Add state with color, radius and throw count

---
+++ FillSpheres/Sphere.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FillSpheres
+{
+    public class Sphere
+    {
+        //Color
+        private Color color;
+        private float radius;
+        private int throwCount;
+    }
+}
